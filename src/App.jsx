94add244be24ecec472560b1ca5397bc8f1b9626
Kulob <mirvonkhugobakhsh@gmail.com>
2023-08-04 21:25:03 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Home from "./pages/home/Home"
-import Login from "./pages/login/Login"
-import Regirter from "./pages/register/Regirter"
-import { useContext } from "react"
-import { AuthContext } from "./context/AuthContext"
-
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Regirter from './pages/register/Regirter';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
-  const {currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
 
-  const ProtectedRoute = ({children}) => {
-    if(!currentUser ) {
-      return <Navigate to='/login'/>
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+    return children;
+  };
   return (
     <Routes>
-      <Route path="/">
-        <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}/>
-        <Route path="login" element={<Login/>}/>
-        <Route path="register" element={<Regirter/>}/>
+      <Route path="/chat">
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Regirter />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
