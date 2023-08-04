4ad36bd329ad6d8fc4b6803f3482aae96a77d6f7
Kulob <mirvonkhugobakhsh@gmail.com>
2023-08-04 21:17:46 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/chat/',
   define: {
     'process.env': {},
   },
-<<<<<<< HEAD
-  base: '/chat/',
-=======
->>>>>>> 74b696ecb0184db7ffe4d4f5543c92d5b87042db
 });
